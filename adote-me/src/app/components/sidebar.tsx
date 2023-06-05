accbec927542e279e84a154d533137dda0990d51
Bruno-Venturini <bruninho_frelo@hotmail.com>
2023-06-05 01:11:15 -0300

    #003 Iniciando criação de componentes e base do design

--- adote-me/src/app/components/sidebar.tsx
+++ adote-me/src/app/components/sidebar.tsx
@@ -1,17 +1,22 @@
-import {Navbar} from "@/app/components/navbar";
-
 export function Sidebar() {
 
     return(
-        <div className="drawer-open">
-            <input id="my-drawer" type="checkbox" className="drawer-toggle"/>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer" className="drawer-overlay"></label>
+        <div className="flex flex-col flex-full h-screen">
+            <div className="flex-initial">
                 <ul className="menu p-4 w-64 h-full bg-base-100 text-base-content">
-                    <button className="btn btn-primary">
-                        Login
-                    </button>
+                    <div>
+                        <div>
+                            <button className="row-span-1 btn btn-accent w-full px-5">
+                                Login
+                            </button>
+                        </div>
+                    </div>
                 </ul>
+            </div>
+            <div className="flex-full mb-20 h-screen w-64 px-5">
+                <div className="bg-primary h-screen artboard phone-sm rounded-lg text-center">
+                    a
+                </div>
             </div>
         </div>
     )
